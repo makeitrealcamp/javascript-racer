fc7e1a4cf304de404001529b132320fafd6392cb
German Hernandez <muygrafico@gmail.com>
2016-09-21 10:18:58 -0500

    add logic to move position up on each player for q and p keystrokes

--- js/game.js
+++ js/game.js
@@ -2,21 +2,49 @@
 $(function() {
   // Handler for .ready() called.
 
-  function proccessKey(key){
+  function moveOnePosition(race, player){
+    race.players[player].position += 1;
+    // console.log(race);
+    console.log(`${player} position: ${race.players[player].position}` );
+    race.status = 'on going';
+  }
+
+  function logArrayElements(element, index, array) {
+    console.log("a[" + index + "] = " + element);
+  }
+
+  function Player( playerID, position ){
+    this.playerID = playerID;
+    this.position = position || 0;
+  }
+
+  function Race(status, players ){
+    this.status = status;
+    this.players = players;
+  }
+
+  function proccessKey(key, race, player, callback){
     if ( event.which == key ) {
-      console.log(`you pressed ${key} code`);
+      callback(race, player);
     }
   }
 
-  function keyStrokesListener(key, callback){
+  function keyStrokesListener(key, race, player, callback){
     $( 'body' ).keyup(function(event) {
-        callback(key, event);
+        callback(key, race, player, moveOnePosition);
     });
   }
 
   function startGame(){
-    keyStrokesListener(81, proccessKey);
-    keyStrokesListener(80, proccessKey);
+    var player1 = new Player('Juan');
+    var player2 = new Player('German');
+    var race = new Race('start', {player1,player2});
+    keyStrokesListener(81, race, 'player1', proccessKey);
+    keyStrokesListener(80, race, 'player2', proccessKey);
+    // keyStrokesListener(80, proccessKey, player);
+
+    console.log(race)
+
   }
 
   startGame();
