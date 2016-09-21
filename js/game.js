4281a6c4bd4ec6229a305c9ec0c865bec422fc67
German Hernandez <muygrafico@gmail.com>
2016-09-21 11:13:57 -0500

    add restart logic

--- js/game.js
+++ js/game.js
@@ -1,32 +1,7 @@
 
 $(function() {
-  // Handler for .ready() called.
 
-  function winner(race,player){
-    if (race.players[player].position >= race.length)
-    {
-      return true;
-    }else{
-      return false;
-    }
-  }
 
-  function moveOnePosition(race, player, callback){
-
-    if ( !callback(race,player) && race.status === 'onGoing' ){
-        race.players[player].position += 1;
-        console.log(`${player} position: ${race.players[player].position}` );
-        // race.status = 'on going';
-    }
-
-    if ( callback(race,player) && race.status === 'onGoing' ){
-        console.log(`${player} is the Winner!`)
-        race.status = 'end';
-    }
-
-    // console.log(race);
-
-  }
 
   function Player( playerID, position ){
     this.playerID = playerID;
@@ -34,9 +9,27 @@ $(function() {
   }
 
   function Race(status, players, length ){
-    this.status = status;
+    this.status = status || 'start';
     this.players = players;
     this.length = length;
+  }
+
+  function winner(race,player){
+    return (race.players[player].position >= race.length) ? true : false;
+  }
+
+  function moveOnePosition(race, player, callback){
+
+    if ( !callback(race,player) && race.status === 'onGoing' ){
+        race.players[player].position += 1;
+        console.log(`${player} position: ${race.players[player].position}` );
+    }
+
+    if ( callback(race,player ) && race.status === 'onGoing' ){
+        console.log(`${race.players[player].playerID} is the Winner!`)
+        race.status = 'end';
+    }
+
   }
 
   function proccessKey(key, race, player, callback){
@@ -45,18 +38,30 @@ $(function() {
     }
   }
 
-  function keyStrokesListener(key, race, player, callback){
+  function keyStrokesListener(key, race, player, callback, callbackCallback){
     $( 'body' ).keyup(function(event) {
-        callback(key, race, player, moveOnePosition);
+        callback(key, race, player, callbackCallback);
+    });
+  }
+
+  function resetGame(race){
+    race.status = 'onGoing';
+    console.log('RESTART GAME!');
+    $.each(race.players, function( index, value ) {
+      race.players[index].position = 0;
     });
   }
 
   function startGame(){
-    var player1 = new Player('Juan');
-    var player2 = new Player('German');
-    var race = new Race('onGoing', {player1,player2}, 20);
-    keyStrokesListener(81, race, 'player1', proccessKey);
-    keyStrokesListener(80, race, 'player2', proccessKey);
+    var p1 = new Player('Juan');
+    var p2 = new Player('German');
+    var p3 = new Player('Carlos');
+    var race = new Race('onGoing', {p1,p2,p3}, 5);
+
+    keyStrokesListener(81, race, 'p1', proccessKey, moveOnePosition);
+    keyStrokesListener(80, race, 'p2', proccessKey, moveOnePosition);
+    keyStrokesListener(85, race, 'p3', proccessKey, moveOnePosition);
+    keyStrokesListener(82, race, '', proccessKey, resetGame);
     // keyStrokesListener(80, proccessKey, player);
 
     console.log(race)
