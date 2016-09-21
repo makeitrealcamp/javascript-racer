bea2eea67e5337e7b8f08231e025c348912c2507
German Hernandez <muygrafico@gmail.com>
2016-09-21 09:27:57 -0500

    add listeners to keys

--- js/game.js
+++ js/game.js
@@ -2,4 +2,23 @@
 $(function() {
   // Handler for .ready() called.
 
+  function proccessKey(key){
+    if ( event.which == key ) {
+      console.log(`you pressed ${key} code`);
+    }
+  }
+
+  function keyStrokesListener(key, callback){
+    $( 'body' ).keyup(function(event) {
+        callback(key, event);
+    });
+  }
+
+  function startGame(){
+    keyStrokesListener(81, proccessKey);
+    keyStrokesListener(80, proccessKey);
+  }
+
+  startGame();
+
 });
