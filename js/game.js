f56b1751a2ae8e75de81bcb34acc4b6ae2a67c8d
German Hernandez <muygrafico@gmail.com>
2016-09-21 12:15:52 -0500

    temp reset with location.reload()

--- js/game.js
+++ js/game.js
@@ -1,8 +1,6 @@
 
 $(function() {
 
-
-
   function Player( playerID, position ){
     this.playerID = playerID;
     this.position = position || 0;
@@ -22,7 +20,8 @@ $(function() {
 
     if ( !callback(race,player) && race.status === 'onGoing' ){
         race.players[player].position += 1;
-        console.log(`${player} position: ${race.players[player].position}` );
+        // console.log(`${player} position: ${race.players[player].position}` );
+        console.log( player +': ' + '-'.repeat( race.players[player].position ) );
     }
 
     if ( callback(race,player ) && race.status === 'onGoing' ){
@@ -45,29 +44,49 @@ $(function() {
   }
 
   function resetGame(race){
+    location.reload();
     race.status = 'onGoing';
     console.log('RESTART GAME!');
+
     $.each(race.players, function( index, value ) {
       race.players[index].position = 0;
     });
   }
 
+  function startCountdown(race){
+
+    if (start === false){
+      var counter = 6;
+      var interval = setInterval(function() {
+          counter--;
+          if (counter !== 0) {console.log(counter)}
+          if (counter == 0) {
+              startGame();
+              clearInterval(interval);
+          }
+      }, 1000);
+    }
+    start = true;
+  }
+
   function startGame(){
+    console.log('Start Game!');
     var p1 = new Player('Juan');
     var p2 = new Player('German');
     var p3 = new Player('Carlos');
-    var race = new Race('onGoing', {p1,p2,p3}, 5);
+    var race = new Race('onGoing', {p1,p2,p3}, 30);
 
     keyStrokesListener(81, race, 'p1', proccessKey, moveOnePosition);
     keyStrokesListener(80, race, 'p2', proccessKey, moveOnePosition);
     keyStrokesListener(85, race, 'p3', proccessKey, moveOnePosition);
     keyStrokesListener(82, race, '', proccessKey, resetGame);
-    // keyStrokesListener(80, proccessKey, player);
-
-    console.log(race)
 
   }
+  console.log('Press start to play!')
+  var start = false;
 
-  startGame();
+    keyStrokesListener(13, '', '', proccessKey, startCountdown);
+
+
 
 });
